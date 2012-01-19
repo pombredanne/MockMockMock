538919eb0a62d4edbe8db9ec93f18f6e24afd5e5
Vincent Jacques <vincent@vincent-jacques.net>
2012-01-19 18:15:45 +0100

    Add an ExpectationProxy to limit publicly visible methods

--- MockMockMock/MockImpl.py
+++ MockMockMock/MockImpl.py
@@ -1,26 +1,5 @@
 from MockException import MockException
 
-class Actionable( object ):
-    def __init__( self ):
-        self.andReturn( None )
-
-    def andReturn( self, value ):
-        self.andExecute( lambda : value )
-        return self
-
-    def andRaise( self, exception ):
-        def Raise(): raise exception
-        self.andExecute( Raise )
-        return self
-
-    def andExecute( self, callable ):
-        self.__action = callable
-        return self
-
-    @property
-    def action( self ):
-        return self.__action
-
 class PropertyCallPolicy( object ):
     @property
     def expectsCall( self ):
@@ -38,19 +17,36 @@ class MethodCallPolicy( object ):
     def checkCall( self, args, kwds ):
         return self.__args == args and self.__kwds == kwds
 
-class Expectation( Actionable ):
+class Expectation( object ):
     def __init__( self, name ):
-        Actionable.__init__( self )
         self.name = name
         self.callPolicy = PropertyCallPolicy()
-
-    def __call__( self, *args, **kwds ):
-        self.callPolicy = MethodCallPolicy( args, kwds )
-        return self
+        self.action = lambda : None
 
     @property
     def expectsCall( self ):
         return self.callPolicy.expectsCall
+
+class ExpectationProxy:
+    def __init__( self, expectation ):
+        self.__expectation = expectation
+
+    def __call__( self, *args, **kwds ):
+        self.__expectation.callPolicy = MethodCallPolicy( args, kwds )
+        return self
+
+    def andReturn( self, value ):
+        self.andExecute( lambda : value )
+        return self
+
+    def andRaise( self, exception ):
+        def Raise(): raise exception
+        self.andExecute( Raise )
+        return self
+
+    def andExecute( self, callable ):
+        self.__expectation.action = callable
+        return self
 
 class Expecter:
     def __init__( self, mock ):
@@ -59,7 +55,7 @@ class Expecter:
     def __getattr__( self, name ):
         expectation = Expectation( name )
         self.__mock.addExpectation( expectation )
-        return expectation
+        return ExpectationProxy( expectation )
 
 class CallChecker:
     def __init__( self, expectation ):
